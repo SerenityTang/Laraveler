46d548f5a9b0a0de94c5b8c30d74fcaf955e4f87
dengzhihao <dengzhihao600@gmail.com>
2018-03-12 18:36:44 +0800

    漏

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -15,13 +15,13 @@ class HomeController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('Dashboard');
-            $content->description('Description...');
+            $content->header('后台主页');
+            $content->description('数据概览');
 
-            $content->row(Dashboard::title());
+            $content->row(Dashboard::title());  //title视图文件
 
             $content->row(function (Row $row) {
-
+                //多少个$row->column()即多少列，第一个参数每一列宽度，与bootstrap一行12列一样
                 $row->column(4, function (Column $column) {
                     $column->append(Dashboard::environment());
                 });
