28e155e48f21192cb7a04cac5a13217c0f357461
dengzhihao <dengzhihao600@gmail.com>
2018-03-12 18:36:29 +0800

    安装laravel-admin后台管理包

--- config/app.php
+++ config/app.php
@@ -164,6 +164,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
+
 
         /*
          * Package Service Providers...
