dd526a97e2a1f43083664b7f2365af619d09966e
Serenity_Tang <1060684139@qq.com>
2018-04-18 13:02:09 +0800

    微信接入路由bug

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['prefix' => 'weixin'], function() {
-    Route::any('/token', 'WeixinController@token');
-    Route::any('/api', 'WeixinController@api');
+    Route::any('/token', ['as' => 'wechat.token', 'Api\WeixinController@token']);
+    Route::any('/api', ['as' => 'wechat.api', 'Api\WeixinController@api']);
 });
