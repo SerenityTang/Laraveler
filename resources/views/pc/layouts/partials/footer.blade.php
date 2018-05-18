a2fe7c6369ff19b84493c8913a065cd30f018e93
Serenity_Tang <1060684139@qq.com>
2018-05-18 22:50:59 +0800

    网站底部完善

--- resources/views/pc/layouts/partials/footer.blade.php
+++ resources/views/pc/layouts/partials/footer.blade.php
@@ -11,23 +11,38 @@
 </div>--}}
 <div class="footer">
     <div class="footer-content">
-        <div class="col-md-6 footer-left">
+        <div class="col-md-5 footer-left">
             <p class="footer-link">
                 <a href="{{ url('/about') }}" class="about" href="">关于Laraveler</a> |
                 <a href="{{ url('/contact') }}" class="contact" href="">联系我们</a> |
                 <a href="{{ url('/help') }}" class="faq" href="">帮助中心</a>
             </p>
-            <p class="copyright">Copyright ©  2018 <a href="/"> laraveler.net</a> | Powered By <a href="http://www.laraveler.net/user/Serenity_Tang">Serenity_Tang</a></p>
-            <p class="record"><img src="imgs/national-emblem.png" alt="国徽" width="15" height="15"> 互联网ICP备案：<a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备18032035号-1 </a></p>
+            <p class="copyright">Copyright ©  2018 <a href="/"  target="_blank"> laraveler.net</a> | Powered By <a href="http://www.laraveler.net/user/Serenity_Tang" target="_blank">Serenity_Tang</a></p>
+            <p class="record"><img src="/imgs/national-emblem.png" alt="国徽" width="15" height="15"> 互联网ICP备案：<a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备18032035号-1 </a></p>
+        </div>
+
+        <div class="col-md-4 footer-middle">
+            <p class="attention-us">关注我们</p>
+            <p class="footer-qrcode">
+                <span class="foooter-official"><img src="/imgs/contact.jpg" alt="官方微信" width="100" height="100"></span>
+                <span class="qr-arrow"><i class="iconfont icon-jiantou32" style="color: #ddd;"></i></span>
+            </p>
+            <p class="footer-qqgroup">
+                <span class="footer-qqgroup-num">QQ群：0123456789</span>
+                <span class="qg-arrow"><i class="iconfont icon-jiantou32" style="color: #ddd;"></i></span>
+            </p>
+
+            <p>
+                <i class="iconfont icon-sr_qq footer-qq"></i>
+                <i class="iconfont icon-weixingongzhonghao1hover footer-weixin"></i>
+                <i class="iconfont icon-weibo-copy footer-weibo"></i>
+                <a href="https://github.com/SerenityTang/Laraveler" target="_blank"><i class="iconfont icon-github footer-github"></i></a>
+            </p>
         </div>
 
         <div class="col-md-3">
-
-        </div>
-
-        <div class="col-md-3">
-            <p>服务赞助商</p>
-            <img src="imgs/ypy.png" alt="又拍云logo" width="80" height="40">
+            <p class="sponsor">服务赞助商</p>
+            <a href="https://www.upyun.com/" target="_blank"><img src="/imgs/ypy.png" alt="又拍云logo" width="90" height="45"></a>
         </div>
     </div>
 </div>
