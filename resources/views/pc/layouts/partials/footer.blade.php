6a9fe17941128f1a0c58d03d610900844b93ad35
Serenity_Tang <1060684139@qq.com>
2018-05-18 16:18:59 +0800

    网站底部排版修改

--- resources/views/pc/layouts/partials/footer.blade.php
+++ resources/views/pc/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="panel footer">
+{{--<div class="panel footer">
     <div class="panel-body">
         <p class="footer-link">
             <a href="{{ url('/about') }}" class="about" href="">关于Laraveler</a> |
@@ -7,5 +7,27 @@
         </p>
         <p class="copyright">互联网ICP备案：<a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备18032035号-1 </a>| Copyright ©  2018 <a href="/"> laraveler.net</a> . All rights reserved.</p>
         <p class="author">Powered By <a href="http://www.laraveler.net/user/Serenity_Tang">Serenity_Tang</a></p>
+    </div>
+</div>--}}
+<div class="footer">
+    <div class="footer-content">
+        <div class="col-md-6 footer-left">
+            <p class="footer-link">
+                <a href="{{ url('/about') }}" class="about" href="">关于Laraveler</a> |
+                <a href="{{ url('/contact') }}" class="contact" href="">联系我们</a> |
+                <a href="{{ url('/help') }}" class="faq" href="">帮助中心</a>
+            </p>
+            <p class="copyright">Copyright ©  2018 <a href="/"> laraveler.net</a> | Powered By <a href="http://www.laraveler.net/user/Serenity_Tang">Serenity_Tang</a></p>
+            <p class="record"><img src="imgs/national-emblem.png" alt="国徽" width="15" height="15"> 互联网ICP备案：<a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备18032035号-1 </a></p>
+        </div>
+
+        <div class="col-md-3">
+
+        </div>
+
+        <div class="col-md-3">
+            <p>服务赞助商</p>
+            <img src="imgs/ypy.png" alt="又拍云logo" width="80" height="40">
+        </div>
     </div>
 </div>
