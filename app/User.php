f4c17575186aeb7d9ce7e181a2ce32e99cbdf576
Serenity_Tang <1060684139@qq.com>
2018-10-09 18:06:31 +0800

    优化注释

--- app/User.php
+++ app/User.php
@@ -95,89 +95,112 @@ class User extends Authenticatable
         return $this->hasMany('App\Models\Answer');
     }
 
-    /*
+    /**
      * 是否回答过此问题
+     *
+     * @param $question_id
+     * @return bool
      */
     public function isAnswer($question_id)
     {
         return boolval($this->answers()->where('question_id', $question_id)->count());
     }
 
-    /*
+    /**
      * 获取用户问答
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function questions()
     {
         return $this->hasMany('App\Models\Question', 'user_id', 'id');
     }
 
-    /*
-    * 获取用户博客
-    */
+    /**
+     * 获取用户博客
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function blogs()
     {
         return $this->hasMany('App\Models\Blog', 'user_id', 'id');
     }
 
-    /*
+    /**
      * 获取用户关注的问答
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function atte_ques()
     {
         return $this->hasMany('App\Models\Attention', 'user_id', 'id')->where('entityable_type', 'App\Models\Question');
     }
 
-    /*
+    /**
      * 获取用户关注的用户
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function atte_user()
     {
         return $this->hasMany('App\Models\Attention', 'user_id', 'id')->where('entityable_type', 'App\User');
     }
 
-    /*
-    * 获取用户收藏的问答
-    */
+    /**
+     * 获取用户收藏的问答
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function coll_ques()
     {
         return $this->hasMany('App\Models\Collection', 'user_id', 'id')->where('entityable_type', 'App\Models\Question');
     }
 
-    /*
-    * 获取用户支持的回答
-    */
+    /**
+     * 获取用户支持的回答
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function supp_answer()
     {
         return $this->hasMany('App\Models\SupportOpposition', 'user_id', 'id')->where('sup_opp_able_type', 'App\Models\Answer')->where('sup_opp_mode', 'support');
     }
 
-    /*
-    * 获取用户反对的回答
-    */
+    /**
+     * 获取用户反对的回答
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function oppo_answer()
     {
         return $this->hasMany('App\Models\SupportOpposition', 'user_id', 'id')->where('sup_opp_able_type', 'App\Models\Answer')->where('sup_opp_mode', 'opposition');
     }
 
-    /*
-    * 获取用户点赞的博客
-    */
+    /**
+     * 获取用户点赞的博客
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function like_blog()
     {
         return $this->hasMany('App\Models\SupportOpposition', 'user_id', 'id')->where('sup_opp_able_type', 'App\Models\Blog')->where('sup_opp_mode', 'like');
     }
 
-    /*
-    * 获取用户点赞的博客
-    */
+    /**
+     * 获取用户点赞的博客
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function coll_blog()
     {
         return $this->hasMany('App\Models\Collection', 'user_id', 'id')->where('entityable_type', 'App\Models\Blog');
     }
 
-    /*
-    * 获取用户邮箱验证的激活信息
-    */
+    /**
+     * 获取用户邮箱验证的激活信息
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function activations()
     {
         return $this->hasOne(\App\Models\UserActivation::class);
