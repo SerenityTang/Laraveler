aca241dc25539b88a7b619dbeee1dadb16dc350e
Serenity_Tang <1060684139@qq.com>
2018-10-15 15:44:28 +0800

    整理注释

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -9,7 +9,7 @@
 Route::any('logout', ['as' => 'auth.logout', 'uses' => 'Auth\LoginController@logout']);
 Route::post('note_verify_code', ['as' => 'auth.note_verify_code', 'uses' => 'Auth\RegisterController@note_verify_code']);
 /*忘记密码*/
-Route::get('/user/forgot', ['as' => 'auth.user.forgot', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);    //返回重置密码表单
-Route::post('mobile_verify_code', ['as' => 'auth.mobile_verify_code', 'uses' => 'Auth\ForgotPasswordController@mobile_verify_code']);//获取手机验证码前验证与验证码发送
-Route::post('forgot_submit', ['as' => 'auth.forgot_submit', 'uses' => 'Auth\ForgotPasswordController@forgot_submit']);  //提交重置密码信息
-Route::post('reset_submit', ['as' => 'auth.reset_submit', 'uses' => 'Auth\ForgotPasswordController@reset_submit']);  //提交新密码
+Route::get('/user/forgot', ['as' => 'auth.user.forgot', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);                    //返回重置密码表单
+Route::post('mobile_verify_code', ['as' => 'auth.mobile_verify_code', 'uses' => 'Auth\ForgotPasswordController@mobile_verify_code']);       //获取手机验证码前验证与验证码发送
+Route::post('forgot_submit', ['as' => 'auth.forgot_submit', 'uses' => 'Auth\ForgotPasswordController@forgot_submit']);                      //提交重置密码信息
+Route::post('reset_submit', ['as' => 'auth.reset_submit', 'uses' => 'Auth\ForgotPasswordController@reset_submit']);                         //提交新密码
--- routes/web/tag.php
+++ routes/web/tag.php
@@ -8,5 +8,5 @@
 Route::group(['prefix' => 'tag'], function () {
     Route::get('/', ['as' => 'tag', 'uses' => 'TagController@index']);
     Route::get('/tag_show/{tag_id}', ['as' => 'tag.tag_show', 'uses' => 'TagController@tag_show']);
-    Route::post('/attention/{tag_id}', ['as' => 'tag.attention', 'uses' => 'TagController@attention']); //关注标签
+    Route::post('/attention/{tag_id}', ['as' => 'tag.attention', 'uses' => 'TagController@attention']);         //关注标签
 });
