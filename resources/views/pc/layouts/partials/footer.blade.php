ba38577c635fe243c083b88ec3310808bba89ce8
SerenityTang <1060684139@qq.com>
2020-01-04 19:49:02 +0800

    修改底部备案信息

--- resources/views/pc/layouts/partials/footer.blade.php
+++ resources/views/pc/layouts/partials/footer.blade.php
@@ -5,7 +5,7 @@
             <a href="{{ url('/contact') }}" class="contact" href="">联系我们</a> |
             <a href="{{ url('/help') }}" class="faq" href="">帮助中心</a>
         </p>
-        <p class="copyright">互联网ICP备案：<a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备18032035号-1 </a>| Copyright ©  2018 <a href="/"> laraveler.net</a> . All rights reserved.</p>
+        <p class="copyright">互联网ICP备案：<a href="http://www.beian.miit.gov.cn" target="_blank">粤ICP备18032035号</a>| Copyright ©  2018 <a href="/"> laraveler.net</a> . All rights reserved.</p>
         <p class="author">Powered By <a href="http://www.laraveler.net/user/Serenity_Tang">Serenity_Tang</a></p>
     </div>
 </div>--}}
@@ -20,7 +20,7 @@
             <p class="copyright">Copyright © 2018 <a href="/" target="_blank"> laraveler.net</a> | Powered By <a
                         href="http://www.laraveler.net/user/Serenity_Tang" target="_blank">Serenity_Tang</a></p>
             <p class="record"><img src="/imgs/national-emblem.png" alt="国徽" width="15" height="15"> 互联网ICP备案：<a
-                        href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备18032035号-1 </a></p>
+                        href="http://www.beian.miit.gov.cn" target="_blank">粤ICP备18032035号</a></p>
         </div>
 
         <div class="col-md-4 footer-middle">
